0252f6b3c5c5ea85d31075535a866c19db4e86ae
carlos-reategui <39816970+carlos-reategui@users.noreply.github.com>
2019-10-20 18:30:12 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 string n1, n2;
 
-unsigned int getValueFromNumber(char number) {
+void getAnswer();
+
+int getValueFromNumber(char number) {
     return number - '0';
 }
 
-unsigned int getValueFromLetter(char letter) {
+int getValueFromLetter(char letter) {
     return letter - 'A' + 10;
 }
 
@@ -24,26 +26,28 @@ int transformedNumberToBase(const string& number, int base){
 int getNumberMinBase(const string& number){
     int base = 1;
     for (char i : number) {
-        if (isdigit(i))
-            base = base > i - '0' ? base : i-'0';
-        else
-            base = base > i - 'A' + 10 ? base : i-'A'+10;
+        isalpha(i) ? base = base > getValueFromLetter(i) ? base : getValueFromLetter(i) :
+                base = base > getValueFromNumber(i) ? base : getValueFromNumber(i);
     }
     return base + 1;
 }
 
+void getAnswer() {
+    int n1Base = getNumberMinBase(n1);
+    int n2Base = getNumberMinBase(n2);
+    bool answerGotten = false;
+    for (; n1Base <= 36 and !answerGotten; ++n1Base)
+        for (int i = n2Base; i <= 36 and !answerGotten; i++) {
+            if (transformedNumberToBase(n1, n1Base) == transformedNumberToBase(n2, i)){
+                answerGotten = true;
+                cout << n1 << " (base " << n1Base << ") = " << n2 << " (base " << i << ")" << endl;
+            }
+        }
+    if (!answerGotten) cout << n1 << " is not equal to " << n2 << " in any base 2..36" << endl;
+}
+
 int main() {
     while (cin >> n1 >> n2){
-        int n1Base = getNumberMinBase(n1);
-        int n2Base = getNumberMinBase(n2);
-        bool answerGotten = false;
-        for (; n1Base <= 36 and !answerGotten; ++n1Base)
-            for (int i = n2Base; i <= 36 and !answerGotten; ++i) {
-                if (transformedNumberToBase(n1, n1Base) == transformedNumberToBase(n2, i)){
-                    answerGotten = true;
-                    cout << n1 << " (base " << n1Base << ") = " << n2 << " (base " << i << ")" << endl;
-                }
-            }
-        if (!answerGotten) cout << n1 << " is not equal to " << n2 << " in any base 2..36" << endl;
+        getAnswer();
     }
 }
